eb70feb07754f44ac85bb77409d04cc7ce92f522
ManavG1005 <78819907+ManavGupta1005@users.noreply.github.com>
2022-10-14 13:07:01 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function updateTime(){
+    n =  new Date();
+    y = n.getFullYear();
+    m = n.getMonth() + 1;
+    d = n.getDate();
+    document.getElementById("date").innerHTML = d + " / " + m + " / " + y;
+
+    h = n.getHours();
+    min = n.getMinutes();
+    s = n.getSeconds();
+    document.getElementById("time").innerHTML = h + " : " + min + " : " + s;
+}
+setInterval(updateTime, 1000);
+function toggleResetPswd(e){
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle() // display:block or none
+    $('#logreg-forms .form-reset').toggle() // display:block or none
+}
+
+function toggleSignUp(e){
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle(); // display:block or none
+    $('#logreg-forms .form-signup').toggle(); // display:block or none
+}
+
+$(()=>{
+    // Login Register Form
+    $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
+    $('#logreg-forms #cancel_reset').click(toggleResetPswd);
+    $('#logreg-forms #btn-signup').click(toggleSignUp);
+    $('#logreg-forms #cancel_signup').click(toggleSignUp);
+})
